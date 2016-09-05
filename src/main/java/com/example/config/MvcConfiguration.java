32f136febad5c3c0c41821f182d2dbaaf6f3b54c
ttinana <ttinana@gmail.com>
2016-09-05 12:52:08 +0200

    .

--- src/main/java/com/example/config/MvcConfiguration.java
+++ src/main/java/com/example/config/MvcConfiguration.java
@@ -4,10 +4,13 @@
 package com.example.config;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.Ordered;
 import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
@@ -17,18 +20,25 @@ import org.springframework.web.servlet.view.InternalResourceViewResolver;
  */
 @Configuration
 @EnableWebMvc
-public class MvcConfiguration extends WebMvcConfigurerAdapter{
+@ComponentScan("org.springframework.security.samples.mvc")
+public class MvcConfiguration extends WebMvcConfigurerAdapter {
     @Bean
     public ViewResolver getViewResolver() {
-        InternalResourceViewResolver resolver = new InternalResourceViewResolver();
-        resolver.setPrefix("/WEB-INF/jsp/");
-        resolver.setSuffix(".jsp");
-        return resolver;
+	InternalResourceViewResolver resolver = new InternalResourceViewResolver();
+	resolver.setPrefix("/WEB-INF/jsp/");
+	resolver.setSuffix(".jsp");
+	return resolver;
     }
-    
+
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+	registry.addViewController("/login").setViewName("login");
+	registry.setOrder(Ordered.HIGHEST_PRECEDENCE);
+    }
+
     @Override
     public void configureDefaultServletHandling(
-            DefaultServletHandlerConfigurer configurer) {
-        configurer.enable();
+	    DefaultServletHandlerConfigurer configurer) {
+	configurer.enable();
     }
 }
--- src/main/java/com/example/config/WebsecurityConfig.java
+++ src/main/java/com/example/config/WebsecurityConfig.java
@@ -3,6 +3,7 @@ package com.example.config;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.*;
 
 @Configuration
@@ -11,7 +12,28 @@ public class WebsecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Autowired
     protected void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
-	auth.inMemoryAuthentication().withUser("user").password("user").roles("USER");
+	auth.inMemoryAuthentication().withUser("admin").password("admin").roles("ADMIN", "USER").and().withUser("user")
+		.password("user").roles("USER");
     }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+	http
+	.authorizeRequests()
+		.antMatchers("/resources/**").permitAll()
+		.anyRequest().authenticated()
+		.and()
+	.formLogin()
+		.loginPage("/login")
+		.permitAll()
+		.and()
+	.logout()
+		.permitAll()
+		.and()
+	.httpBasic();
+	//super.configure(http);
+    }
+    
+    
 
 }
